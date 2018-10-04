b95af1f2f339f3eccee76eb3ab20267be781b0ab
Nitish Victor <nithishvictor@gmail.com>
2018-10-04 16:25:32 -0400

    Use DirectXTK to load textures

--- ElixirEngine/Core/Core.cpp
+++ ElixirEngine/Core/Core.cpp
@@ -305,107 +305,20 @@ void Core::InitResources()
 	pixelCb.light.DiffuseColor = XMFLOAT4(1.f, 0.0f, 0.f, 0.f);
 	pixelCb.light.Direction = XMFLOAT3(1.f, 0.f, 0.f);
 
-
-
 	D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
 	heapDesc.NumDescriptors = 1;
 	heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
 	heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
 	device->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&mainDescriptorHeap));
 
-	BYTE *imageData;
-	// Load the image from file
-	D3D12_RESOURCE_DESC textureDesc;
-	int imageBytesPerRow;
-	int imageSize = LoadImageDataFromFile(&imageData, textureDesc, L"../../Assets/metal.jpg", imageBytesPerRow);
-
-	// make sure we have data
-	if (imageSize <= 0)
-	{
-		Running = false;
-	}
-
-	//// create a default heap where the upload heap will copy its contents into (contents being the texture)
-	device->CreateCommittedResource(
-		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT), // a default heap
-		D3D12_HEAP_FLAG_NONE, // no flags
-		&textureDesc, // the description of our texture
-		D3D12_RESOURCE_STATE_COPY_DEST, // We will copy the texture from the upload heap to here, so we start it out in a copy dest state
-		nullptr, // used for render targets and depth/stencil buffers
-		IID_PPV_ARGS(&textureBuffer));
-
-	textureBuffer->SetName(L"Texture Buffer Resource Heap");
-
-	UINT64 textureUploadBufferSize;
-
-	// this function gets the size an upload buffer needs to be to upload a texture to the gpu.
-	device->GetCopyableFootprints(&textureDesc, 0, 1, 0, nullptr, nullptr, nullptr, &textureUploadBufferSize);
-	// now we create an upload heap to upload our texture to the GPU
-	device->CreateCommittedResource(
-		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD), // upload heap
-		D3D12_HEAP_FLAG_NONE, // no flags
-		&CD3DX12_RESOURCE_DESC::Buffer(textureUploadBufferSize), // resource description for a buffer (storing the image data in this heap just to copy to the default heap)
-		D3D12_RESOURCE_STATE_GENERIC_READ, // We will copy the contents from this heap to the default heap above
-		nullptr,
-		IID_PPV_ARGS(&textureBufferUploadHeap));
-
-	textureBufferUploadHeap->SetName(L"Texture Buffer Upload Resource Heap");
-
-	// store vertex buffer in upload heap
-	D3D12_SUBRESOURCE_DATA textureData = {};
-	textureData.pData = &imageData[0]; // pointer to our image data
-	textureData.RowPitch = imageBytesPerRow; // size of all our triangle vertex data
-	textureData.SlicePitch = imageBytesPerRow * textureDesc.Height; // also the size of our triangle vertex data
-
-																	// Now we copy the upload buffer contents to the default heap
-	UpdateSubresources(commandList, textureBuffer, textureBufferUploadHeap, 0, 0, 1, &textureData);
-
-	// transition the texture default heap to a pixel shader resource (we will be sampling from this heap in the pixel shader to get the color of pixels)
-	
-
-	// now we create a shader resource view (descriptor that points to the texture and describes it)
-	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
-	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-	srvDesc.Format = textureDesc.Format;
-	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-	srvDesc.Texture2D.MipLevels = 1;
-	deferredRenderer->SetSRV(textureBuffer, textureDesc.Format, 0);
-
-	delete imageData;
-
-	imageSize = LoadImageDataFromFile(&imageData, textureDesc, L"../../Assets/metalNormal.png", imageBytesPerRow);
-
-	//device->CreateCommittedResource(
-	//	&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
-	//	D3D12_HEAP_FLAG_NONE,
-	//	&textureDesc,
-	//	D3D12_RESOURCE_STATE_COPY_DEST,
-	//	nullptr,
-	//	IID_PPV_ARGS(&normalTexture));
-	//device->GetCopyableFootprints(&textureDesc, 0, 1, 0, nullptr, nullptr, nullptr, &textureUploadBufferSize);
-	//device->CreateCommittedResource(
-	//	&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
-	//	D3D12_HEAP_FLAG_NONE, // no flags
-	//	&CD3DX12_RESOURCE_DESC::Buffer(textureUploadBufferSize),
-	//	D3D12_RESOURCE_STATE_GENERIC_READ,
-	//	nullptr,
-	//	IID_PPV_ARGS(&textureBufferUploadHeap));
-
-	//textureData.pData = &imageData[0];
-	//textureData.RowPitch = imageBytesPerRow;
-	//textureData.SlicePitch = imageBytesPerRow * textureDesc.Height;
-
-	//UpdateSubresources(commandList, normalTexture, textureBufferUploadHeap, 0, 0, 1, &textureData);
-
-
 	uploadBatch.Begin();
-	//CreateWICTextureFromFile(device, uploadBatch, L"../../Assets/metal.jpg", &textureBuffer, true);
+	CreateWICTextureFromFile(device, uploadBatch, L"../../Assets/metal.jpg", &textureBuffer, false);
 	CreateWICTextureFromFile(device, uploadBatch, L"../../Assets/metalNormal.png", &normalTexture, true);
 	auto uploadOperation = uploadBatch.End(commandQueue);
 	uploadOperation.wait();
 
 	//commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(normalTexture, D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE));
-	//deferredRenderer->SetSRV(textureBuffer, DXGI_FORMAT_R11G11B10_FLOAT, 0);
+	deferredRenderer->SetSRV(textureBuffer, DXGI_FORMAT_R8G8B8A8_UNORM, 0);
 	deferredRenderer->SetSRV(normalTexture, DXGI_FORMAT_B8G8R8A8_UNORM, 1);
 	//device->CreateShaderResourceView(textureBuffer, &srvDesc, mainDescriptorHeap->GetCPUDescriptorHandleForHeapStart());
 
@@ -424,8 +337,6 @@ void Core::InitResources()
 	{
 		Running = false;
 	}
-
-	delete imageData;
 
 	// Fill out the Viewport
 	viewport.TopLeftX = 0;
@@ -563,7 +474,7 @@ void Core::Cleanup()
 	depthStencilBuffer->Release();
 	dsDescriptorHeap->Release();
 	textureBuffer->Release();
-	textureBufferUploadHeap->Release();
+	//textureBufferUploadHeap->Release();
 
 	delete mesh;
 	delete camera;
