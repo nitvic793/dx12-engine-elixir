57e8d230b0900cc1d8fa833ea0e4b2a088b14b3a
Nitish Victor <nithishvictor@gmail.com>
2019-01-25 19:44:15 -0500

    Separate Engine from App

--- ElixirEngine/Main.cpp
+++ ElixirEngine/Main.cpp
@@ -1,40 +1,40 @@
 #include "stdafx.h"
-#include "Game.h"
-//#include <vld.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
-{
-#if defined(DEBUG) | defined(_DEBUG)
-	// Enable memory leak detection - quick and dirty
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-	// Ensure "Current Directory" (relative path) is always the .exe's folder
-	{
-		char currentDir[1024] = {};
-		GetModuleFileName(0, currentDir, 1024);
-		char* lastSlash = strrchr(currentDir, '\\');
-		if (lastSlash)
-		{
-			*lastSlash = 0; 
-			SetCurrentDirectory(currentDir);
-		}
-	}
-
-	Game* game = new Game(hInstance, nCmdShow, 1920, 1080, false);
-	game->Initialize();
-	game->Run([&](Core* core) 
-	{
-	});
-	game->Shutdown();
-	delete game;
-
-	IDXGIDebug1* pDebug = nullptr;
-	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&pDebug))))
-	{
-		
-		pDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_SUMMARY);
-		pDebug->Release();
-	}
-
-	return 0;
-}
+//#include "Game.h"
+////#include <vld.h>
+//
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+//{
+//#if defined(DEBUG) | defined(_DEBUG)
+//	// Enable memory leak detection - quick and dirty
+//	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+//#endif
+//	// Ensure "Current Directory" (relative path) is always the .exe's folder
+//	{
+//		char currentDir[1024] = {};
+//		GetModuleFileName(0, currentDir, 1024);
+//		char* lastSlash = strrchr(currentDir, '\\');
+//		if (lastSlash)
+//		{
+//			*lastSlash = 0; 
+//			SetCurrentDirectory(currentDir);
+//		}
+//	}
+//
+//	Game* game = new Game(hInstance, nCmdShow, 1920, 1080, false);
+//	game->Initialize();
+//	game->Run([&](Core* core) 
+//	{
+//	});
+//	game->Shutdown();
+//	delete game;
+//
+//	IDXGIDebug1* pDebug = nullptr;
+//	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&pDebug))))
+//	{
+//		
+//		pDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_SUMMARY);
+//		pDebug->Release();
+//	}
+//
+//	return 0;
+//}
---
+++ Engine.App/Elixir.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
+#endif
+#define NOMINMAX
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <string>
+#include <wincodec.h>
+#include <dxgidebug.h>
+
